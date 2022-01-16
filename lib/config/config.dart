14154961f08aaa9e8ea1ad38660fb76f7b13ff49
arslan9380 <73348837+arslan9380@users.noreply.github.com>
2022-01-16 12:19:48 +0500

    intial commit

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'Travel Hour'; //app name
+  final String testerPassword = 'tester';  //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAA1cgQa-M:APA91bFMD4KswezZfuap6nN******************';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
